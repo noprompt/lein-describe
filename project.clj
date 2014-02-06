1b67791301e375225539d2ec656e86403de3f316
Joel Holdbrooks <cjholdbrooks@gmail.com>
2014-02-05 22:09:57 -0800

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-describe "0.3.0-SNAPSHOT"
   :description "A Leiningen plugin for displaying detailed project information."
-  :url "https://github.com/noprompt/lein-garden"
+  :url "https://github.com/noprompt/lein-describe"
   :license {:name "Unlicense"
             :url "http://unlicense.org/UNLICENSE"}
   :eval-in-leiningen true)
